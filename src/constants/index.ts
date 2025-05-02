e7278ae7a465df4e081c2e4f94c704bac8775df4
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2025-05-03 00:12:41 +0700

    feat: share resource modal

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,10 +2,12 @@ import Terminal from "../assets/icons/terminal.svg";
 import Link from "../assets/icons/link.svg";
 import OpenInNew from "../assets/icons/open_in_new.svg";
 import Delete from "../assets/icons/delete.svg";
+import Share from "../assets/icons/share.svg";
 
 export const ICONS = {
   terminal: Terminal,
   link: Link,
   openInNew: OpenInNew,
   delete: Delete,
+  share: Share,
 };
