82e33c272270003e532dd8f8b1faa010c6e9cd71
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2025-05-02 23:50:23 +0700

    feat: modal to see shared to

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+import Terminal from "../assets/icons/terminal.svg";
+import Link from "../assets/icons/link.svg";
+import OpenInNew from "../assets/icons/open_in_new.svg";
+import Delete from "../assets/icons/delete.svg";
+
+export const ICONS = {
+  terminal: Terminal,
+  link: Link,
+  openInNew: OpenInNew,
+  delete: Delete,
+};
