a4edf2455878957e7b5cc979d3067344a95a88ca
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2025-05-07 02:15:42 +0700

    feat: protected admin route

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,20 @@ export const onRequest: MiddlewareHandler = async (Astro, next) => {
   const user = await Astro?.session?.get("user");
   const isLoginPage = ["/login", "/login-admin"].includes(Astro.url.pathname);
   const isDashboardRoute = Astro.url.pathname.startsWith("/dashboard");
-
+  const isAdminPage = [
+    "/dashboard/settings/eligible-users",
+    "/dashboard/settings/eligible-users/modal-eligible-users",
+    "/dashboard/settings/eligible-users/delete-eligible-users",
+  ].includes(Astro.url.pathname);
   if (user) {
     Astro.locals.user = user;
   }
 
   if (user && isLoginPage) {
+    return Astro.redirect("/dashboard/resources");
+  }
+
+  if (user && isAdminPage && user.role !== "ADMIN") {
     return Astro.redirect("/dashboard/resources");
   }
 
